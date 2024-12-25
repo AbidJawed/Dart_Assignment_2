a53d093908e3c7e59357dcd55b7f3a2dc1b7582b
AbidJawed <abid.jawed993@gmail.com>
2024-12-25 02:21:38 -0800

    Create an empty list of type string called days. Use the add method to add // names of 7 days and print all days.

--- Q1.dart
+++ Q1.dart
@@ -1,4 +1,4 @@
-//Q1: Print a List of Names
+  // Q1: Create a list of names and print all names using list.
 void main() {
   List<String> names = ["Abid", "Zeeshan", "Faizan"];
   //github
@@ -6,3 +6,5 @@ void main() {
       (name) => print(name)); //har iteration ke liye custom logic chaiye ho
   // names.forEach(print); sirf ek print function pass karna ho
 }
+
+
