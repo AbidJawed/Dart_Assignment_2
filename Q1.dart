2965fa0569ea7fc3cd011f58b5f4deb829b3522e
AbidJawed <abid.jawed993@gmail.com>
2024-12-25 02:09:51 -0800

    Print a List of Names

---
+++ Q1.dart
@@ -0,0 +1,8 @@
+//Q1: Print a List of Names
+void main() {
+  List<String> names = ["Abid", "Zeeshan", "Faizan"];
+  //github
+  names.forEach(
+      (name) => print(name)); //har iteration ke liye custom logic chaiye ho
+  // names.forEach(print); sirf ek print function pass karna ho
+}
