0b1dd2becc1563eaaf6ef6156b651ab7a5656644
AbidJawed <abid.jawed993@gmail.com>
2024-12-25 02:41:33 -0800

    Q3: Create a list of Days and remove one by one from the end of list.

---
+++ Q3.dart
@@ -0,0 +1,15 @@
+// Q3: Create a list of Days and remove one by one from the end of list.
+
+void main() {
+  List days = [];
+  days.addAll([
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "sunday"
+  ]);
+  print(days);
+}
