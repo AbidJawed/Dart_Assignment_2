e076763f08814dcdc4edd3c7aaee3ffc3f5507e5
AbidJawed <abid.jawed993@gmail.com>
2024-12-25 09:09:19 -0800

    Q.8: remove all false values from below list by using removeWhere or // retainWhere property.

---
+++ Q7.dart
@@ -0,0 +1,26 @@
+//   Q.7:
+// Map<String, double> expenses = {
+// 'sun': 3000.0,
+// 'mon': 3000.0,
+// 'tue': 3234.0,
+// };
+// Check if "fri" exist in expanses; if exist change it's value to 5000.0 otherwise
+// add 'fri' to expenses and set its value to 5000.0 then print expenses.
+
+void main() {
+  Map<String, double> expenses = {
+    'sun': 3000.0,
+    'mon': 3000.0,
+    'tue': 3234.0,
+  };
+  bool exist = expenses.containsKey('fri');
+  if (exist) {
+    print("fri is availble");
+  } else {
+    // expenses.putIfAbsent('fri', () => 5000.0);
+    // expenses['fri'] = 5000.0;
+    // expenses.update('fri', (value) => 5000.0);
+    expenses.addAll({'fri': 5000.0});
+  }
+  print(expenses);
+}
